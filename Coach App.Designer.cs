2ad0bed21ca18d770b94fc816bb66a3363dfb832
Gabriel Da Silva <N0726678@my.ntu.ac.uk>
2021-02-15 17:41:38 +0000

    Second Commit
    Added different features and colours to the design of the system.

---
+++ Coach App.Designer.cs
@@ -0,0 +1,301 @@
+﻿
+namespace Coach_Form_UI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MainPannel = new System.Windows.Forms.Panel();
+            this.AboutUsBtn = new FontAwesome.Sharp.IconButton();
+            this.AccountBtn = new FontAwesome.Sharp.IconButton();
+            this.BookTicketBtn = new FontAwesome.Sharp.IconButton();
+            this.HomeBtn = new FontAwesome.Sharp.IconButton();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.UserNameLabel = new System.Windows.Forms.Label();
+            this.iconPictureBox1 = new FontAwesome.Sharp.IconPictureBox();
+            this.TopMenuPanel = new System.Windows.Forms.Panel();
+            this.MiniBtn = new FontAwesome.Sharp.IconButton();
+            this.CloseBtn = new FontAwesome.Sharp.IconButton();
+            this.IconChild = new FontAwesome.Sharp.IconPictureBox();
+            this.ChildLabel = new System.Windows.Forms.Label();
+            this.MainPannel.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).BeginInit();
+            this.TopMenuPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.IconChild)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MainPannel
+            // 
+            this.MainPannel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.MainPannel.Controls.Add(this.AboutUsBtn);
+            this.MainPannel.Controls.Add(this.AccountBtn);
+            this.MainPannel.Controls.Add(this.BookTicketBtn);
+            this.MainPannel.Controls.Add(this.HomeBtn);
+            this.MainPannel.Controls.Add(this.panel2);
+            this.MainPannel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.MainPannel.Location = new System.Drawing.Point(0, 0);
+            this.MainPannel.Name = "MainPannel";
+            this.MainPannel.Size = new System.Drawing.Size(391, 1117);
+            this.MainPannel.TabIndex = 0;
+            // 
+            // AboutUsBtn
+            // 
+            this.AboutUsBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.AboutUsBtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.AboutUsBtn.FlatAppearance.BorderSize = 0;
+            this.AboutUsBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.AboutUsBtn.Font = new System.Drawing.Font("Nirmala UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AboutUsBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.AboutUsBtn.IconChar = FontAwesome.Sharp.IconChar.Building;
+            this.AboutUsBtn.IconColor = System.Drawing.Color.White;
+            this.AboutUsBtn.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.AboutUsBtn.IconSize = 80;
+            this.AboutUsBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.AboutUsBtn.Location = new System.Drawing.Point(0, 626);
+            this.AboutUsBtn.Name = "AboutUsBtn";
+            this.AboutUsBtn.Size = new System.Drawing.Size(391, 110);
+            this.AboutUsBtn.TabIndex = 4;
+            this.AboutUsBtn.Text = "About Us";
+            this.AboutUsBtn.UseVisualStyleBackColor = false;
+            this.AboutUsBtn.Click += new System.EventHandler(this.AbountUsBtn_Click);
+            // 
+            // AccountBtn
+            // 
+            this.AccountBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.AccountBtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.AccountBtn.FlatAppearance.BorderSize = 0;
+            this.AccountBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.AccountBtn.Font = new System.Drawing.Font("Nirmala UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AccountBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.AccountBtn.IconChar = FontAwesome.Sharp.IconChar.User;
+            this.AccountBtn.IconColor = System.Drawing.Color.White;
+            this.AccountBtn.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.AccountBtn.IconSize = 80;
+            this.AccountBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.AccountBtn.Location = new System.Drawing.Point(0, 516);
+            this.AccountBtn.Name = "AccountBtn";
+            this.AccountBtn.Size = new System.Drawing.Size(391, 110);
+            this.AccountBtn.TabIndex = 3;
+            this.AccountBtn.Text = "Account";
+            this.AccountBtn.UseVisualStyleBackColor = false;
+            this.AccountBtn.Click += new System.EventHandler(this.Account_Click);
+            // 
+            // BookTicketBtn
+            // 
+            this.BookTicketBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.BookTicketBtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.BookTicketBtn.FlatAppearance.BorderSize = 0;
+            this.BookTicketBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BookTicketBtn.Font = new System.Drawing.Font("Nirmala UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BookTicketBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.BookTicketBtn.IconChar = FontAwesome.Sharp.IconChar.TicketAlt;
+            this.BookTicketBtn.IconColor = System.Drawing.Color.White;
+            this.BookTicketBtn.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.BookTicketBtn.IconSize = 80;
+            this.BookTicketBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BookTicketBtn.Location = new System.Drawing.Point(0, 406);
+            this.BookTicketBtn.Name = "BookTicketBtn";
+            this.BookTicketBtn.Size = new System.Drawing.Size(391, 110);
+            this.BookTicketBtn.TabIndex = 2;
+            this.BookTicketBtn.Text = "Book Ticket";
+            this.BookTicketBtn.UseVisualStyleBackColor = false;
+            this.BookTicketBtn.Click += new System.EventHandler(this.BookTicketBtn_Click);
+            // 
+            // HomeBtn
+            // 
+            this.HomeBtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.HomeBtn.FlatAppearance.BorderSize = 0;
+            this.HomeBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.HomeBtn.Flip = FontAwesome.Sharp.FlipOrientation.Horizontal;
+            this.HomeBtn.Font = new System.Drawing.Font("Nirmala UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HomeBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.HomeBtn.IconChar = FontAwesome.Sharp.IconChar.Home;
+            this.HomeBtn.IconColor = System.Drawing.Color.White;
+            this.HomeBtn.IconFont = FontAwesome.Sharp.IconFont.Solid;
+            this.HomeBtn.IconSize = 80;
+            this.HomeBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.HomeBtn.Location = new System.Drawing.Point(0, 296);
+            this.HomeBtn.Name = "HomeBtn";
+            this.HomeBtn.Size = new System.Drawing.Size(391, 110);
+            this.HomeBtn.TabIndex = 1;
+            this.HomeBtn.Text = "Home";
+            this.HomeBtn.UseVisualStyleBackColor = true;
+            this.HomeBtn.Click += new System.EventHandler(this.HomeBtn_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.UserNameLabel);
+            this.panel2.Controls.Add(this.iconPictureBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(391, 296);
+            this.panel2.TabIndex = 0;
+            // 
+            // UserNameLabel
+            // 
+            this.UserNameLabel.AutoSize = true;
+            this.UserNameLabel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.UserNameLabel.Font = new System.Drawing.Font("Nirmala UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UserNameLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(156)))), ((int)(((byte)(249)))));
+            this.UserNameLabel.Location = new System.Drawing.Point(106, 232);
+            this.UserNameLabel.Name = "UserNameLabel";
+            this.UserNameLabel.Size = new System.Drawing.Size(165, 41);
+            this.UserNameLabel.TabIndex = 2;
+            this.UserNameLabel.Text = "User Name";
+            // 
+            // iconPictureBox1
+            // 
+            this.iconPictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.iconPictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.iconPictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.iconPictureBox1.IconChar = FontAwesome.Sharp.IconChar.UserCircle;
+            this.iconPictureBox1.IconColor = System.Drawing.Color.White;
+            this.iconPictureBox1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconPictureBox1.IconSize = 196;
+            this.iconPictureBox1.Location = new System.Drawing.Point(88, 29);
+            this.iconPictureBox1.Name = "iconPictureBox1";
+            this.iconPictureBox1.Size = new System.Drawing.Size(196, 200);
+            this.iconPictureBox1.TabIndex = 0;
+            this.iconPictureBox1.TabStop = false;
+            // 
+            // TopMenuPanel
+            // 
+            this.TopMenuPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.TopMenuPanel.Controls.Add(this.ChildLabel);
+            this.TopMenuPanel.Controls.Add(this.IconChild);
+            this.TopMenuPanel.Controls.Add(this.MiniBtn);
+            this.TopMenuPanel.Controls.Add(this.CloseBtn);
+            this.TopMenuPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopMenuPanel.Location = new System.Drawing.Point(391, 0);
+            this.TopMenuPanel.Name = "TopMenuPanel";
+            this.TopMenuPanel.Size = new System.Drawing.Size(2114, 89);
+            this.TopMenuPanel.TabIndex = 1;
+            this.TopMenuPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TopMenuPanel_MouseDown);
+            // 
+            // MiniBtn
+            // 
+            this.MiniBtn.Dock = System.Windows.Forms.DockStyle.Right;
+            this.MiniBtn.FlatAppearance.BorderSize = 0;
+            this.MiniBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.MiniBtn.IconChar = FontAwesome.Sharp.IconChar.WindowMinimize;
+            this.MiniBtn.IconColor = System.Drawing.Color.White;
+            this.MiniBtn.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.MiniBtn.IconSize = 90;
+            this.MiniBtn.Location = new System.Drawing.Point(1958, 0);
+            this.MiniBtn.Name = "MiniBtn";
+            this.MiniBtn.Size = new System.Drawing.Size(78, 89);
+            this.MiniBtn.TabIndex = 1;
+            this.MiniBtn.UseVisualStyleBackColor = true;
+            this.MiniBtn.Click += new System.EventHandler(this.MiniBtn_Click);
+            // 
+            // CloseBtn
+            // 
+            this.CloseBtn.Dock = System.Windows.Forms.DockStyle.Right;
+            this.CloseBtn.FlatAppearance.BorderSize = 0;
+            this.CloseBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CloseBtn.IconChar = FontAwesome.Sharp.IconChar.WindowClose;
+            this.CloseBtn.IconColor = System.Drawing.Color.White;
+            this.CloseBtn.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.CloseBtn.IconSize = 90;
+            this.CloseBtn.Location = new System.Drawing.Point(2036, 0);
+            this.CloseBtn.Name = "CloseBtn";
+            this.CloseBtn.Size = new System.Drawing.Size(78, 89);
+            this.CloseBtn.TabIndex = 0;
+            this.CloseBtn.UseVisualStyleBackColor = true;
+            this.CloseBtn.Click += new System.EventHandler(this.CloseBtn_Click);
+            // 
+            // IconChild
+            // 
+            this.IconChild.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.IconChild.Dock = System.Windows.Forms.DockStyle.Left;
+            this.IconChild.Flip = FontAwesome.Sharp.FlipOrientation.Horizontal;
+            this.IconChild.IconChar = FontAwesome.Sharp.IconChar.Home;
+            this.IconChild.IconColor = System.Drawing.Color.White;
+            this.IconChild.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.IconChild.IconSize = 88;
+            this.IconChild.Location = new System.Drawing.Point(0, 0);
+            this.IconChild.Name = "IconChild";
+            this.IconChild.Size = new System.Drawing.Size(88, 89);
+            this.IconChild.TabIndex = 2;
+            this.IconChild.TabStop = false;
+            // 
+            // ChildLabel
+            // 
+            this.ChildLabel.AutoSize = true;
+            this.ChildLabel.Font = new System.Drawing.Font("Nirmala UI", 9.900001F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ChildLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.ChildLabel.Location = new System.Drawing.Point(94, 19);
+            this.ChildLabel.Name = "ChildLabel";
+            this.ChildLabel.Size = new System.Drawing.Size(116, 46);
+            this.ChildLabel.TabIndex = 3;
+            this.ChildLabel.Text = "Home";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(2505, 1117);
+            this.Controls.Add(this.TopMenuPanel);
+            this.Controls.Add(this.MainPannel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.MainPannel.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).EndInit();
+            this.TopMenuPanel.ResumeLayout(false);
+            this.TopMenuPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.IconChild)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel MainPannel;
+        private System.Windows.Forms.Panel panel2;
+        private FontAwesome.Sharp.IconButton HomeBtn;
+        private System.Windows.Forms.Label UserNameLabel;
+        private FontAwesome.Sharp.IconPictureBox iconPictureBox1;
+        private FontAwesome.Sharp.IconButton BookTicketBtn;
+        private FontAwesome.Sharp.IconButton AccountBtn;
+        private FontAwesome.Sharp.IconButton AboutUsBtn;
+        private System.Windows.Forms.Panel TopMenuPanel;
+        private FontAwesome.Sharp.IconButton CloseBtn;
+        private FontAwesome.Sharp.IconButton MiniBtn;
+        private FontAwesome.Sharp.IconPictureBox IconChild;
+        private System.Windows.Forms.Label ChildLabel;
+    }
+}
+
---
+++ Coach App.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using FontAwesome.Sharp;
+
+namespace Coach_Form_UI
+{
+    public partial class Form1 : Form
+    {
+        private IconButton currentBtn;
+        private Panel leftBtnMenu;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            leftBtnMenu = new Panel();
+            leftBtnMenu.Size = new Size(7, 100);
+            MainPannel.Controls.Add(leftBtnMenu);
+
+            //Main form
+            this.DoubleBuffered = true;
+            this.ControlBox = false;
+            this.MaximizedBounds = Screen.FromHandle(this.Handle).WorkingArea;
+
+
+        }
+
+        private struct RGBColors
+        {
+            public static Color color1 = Color.FromArgb(172, 126, 241);
+            public static Color color2 = Color.FromArgb(249, 118, 176);
+            public static Color color3 = Color.FromArgb(253, 138, 114);
+            public static Color color4 = Color.FromArgb(95, 77, 221);
+            public static Color color5 = Color.FromArgb(249, 88, 155);
+            public static Color color6 = Color.FromArgb(24, 161, 251);
+        }
+
+        private void activeBtn(object senderBtn, Color color)
+        {
+            if (senderBtn != null)
+            {
+                DeactivateBtn();
+                // btn
+                currentBtn = (IconButton)senderBtn;
+                currentBtn.BackColor = Color.FromArgb(37, 36, 81);
+                currentBtn.ForeColor = color;
+                currentBtn.TextAlign = ContentAlignment.MiddleCenter;
+                currentBtn.IconColor = color;
+                currentBtn.TextImageRelation = TextImageRelation.TextBeforeImage;
+                currentBtn.ImageAlign = ContentAlignment.MiddleRight;
+
+                //left btn color
+                leftBtnMenu.BackColor = color;
+                leftBtnMenu.Location = new Point(0, currentBtn.Location.Y);
+                leftBtnMenu.Visible = true;
+                leftBtnMenu.BringToFront();
+
+                //child btn
+                IconChild.IconChar = currentBtn.IconChar;
+                IconChild.IconColor = color;
+                ChildLabel.Text = currentBtn.Text;
+                ChildLabel.ForeColor = color;
+
+            }
+        
+        }
+
+        private void DeactivateBtn()
+        {
+            if (currentBtn != null)
+            {
+                currentBtn.BackColor = Color.FromArgb(24, 30, 54);
+                currentBtn.ForeColor = Color.FromArgb(0, 126, 249);
+                currentBtn.TextAlign = ContentAlignment.MiddleCenter;
+                currentBtn.IconColor = Color.White;
+                currentBtn.TextImageRelation = TextImageRelation.TextBeforeImage;
+                currentBtn.ImageAlign = ContentAlignment.MiddleLeft;
+
+            }
+            
+        }
+
+
+
+
+        private void HomeBtn_Click(object sender, EventArgs e)
+        {
+            activeBtn(sender, RGBColors.color1);
+
+            
+        }
+
+        private void BookTicketBtn_Click(object sender, EventArgs e)
+        {
+            activeBtn(sender, RGBColors.color2);
+        }
+
+        private void Account_Click(object sender, EventArgs e)
+        {
+            activeBtn(sender, RGBColors.color3);
+        }
+
+        private void AbountUsBtn_Click(object sender, EventArgs e)
+        {
+            activeBtn(sender, RGBColors.color4);
+        }
+
+
+        private void CloseBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MiniBtn_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        //Drag menu
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+        private void TopMenuPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+    }
+}
