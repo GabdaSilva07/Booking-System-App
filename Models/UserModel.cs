9be80956ff5c0ea0360d7ad4f84cf7061eac2726
Gabriel Da Silva <N0726678@my.ntu.ac.uk>
2021-03-18 20:28:25 +0000

    Login is working.

---
+++ Models/UserModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coach_Form_UI
+{
+    class UserModel
+    {
+        private string firstName;
+        private string surname;
+        private string email;
+        private int age;
+        private string password;
+
+
+        public UserModel()
+        {
+        }
+
+        public UserModel(string firstName, string surname, string email, int age, string password)
+        {
+            FirstName = firstName;
+            Surname = surname;
+            Email = email;
+            Age = age;
+            Password = password;
+        }
+
+        public string FirstName 
+        { get { return firstName; } 
+          set { firstName = value; } 
+        }
+        public string Surname
+        {
+            get { return surname; }
+            set { surname = value; }
+        }
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age= value; }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+
+    }
+}
